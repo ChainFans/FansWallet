dc2ed1b4879ee1a26817ffd23c42e354cc1d9da7
陈立 <chenli@chainfans.io>
2020-11-24 14:53:02 +0800

    Interface optimization

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-     <!-- :style="{'padding-top':$store.state.appTop }" -->
-    <!-- <div :style="{height:$store.state.appTop+'px',background:'#eee'}"></div> -->
     <keep-alive>
       <router-view v-if="$route.meta.keepAlive"></router-view>
     </keep-alive>   
